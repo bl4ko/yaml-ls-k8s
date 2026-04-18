036e7e2e5d19b3507c4777f3f503e91330159f07
Ren0v8r <184384955+Ren0v8r@users.noreply.github.com>
2026-04-18 22:03:06 +0000

    chore(deps): update kubernetes schema to v1.35.4

--- internal/config/config.go
+++ internal/config/config.go
@@ -18,7 +18,7 @@ func DefaultConfig() *Config {
 		YamlLSPath: "yaml-language-server",
 		LogFile:    filepath.Join(home, ".config", "yaml-ls-k8s", "server.log"),
 		// renovate: datasource=github-releases depName=kubernetes/kubernetes
-		K8sVersion: "v1.35.3",
+		K8sVersion: "v1.35.4",
 		CacheDir:   filepath.Join(home, ".cache", "yaml-ls-k8s", "schemas"),
 	}
 }
